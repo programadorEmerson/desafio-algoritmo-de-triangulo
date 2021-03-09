5cf54f4ac0f1660d0fb5629ac2539575a05ba6d4
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-09 09:25:28 -0300

    Desafio Triangulo Trybe Alterações

--- script.js
+++ script.js
@@ -6,44 +6,41 @@ window.onload = function() {
   let trianguloRecuperado = document.querySelector(".triangle");
   let linhasDoHtml = null;
 
-  montarLinhas();
-  atualizarContador();
-  preenchertriangulo(linhasDoHtml);
+  montarLinhas() && preencherTriangulo(linhasDoHtml);  
 
   function montarLinhas(){
     for (let index = 0; index < NumerosDeLinhasDoTriangulo; index++) {
-      let linhas = document.createElement("div");
-      linhas.className = "line";
-      trianguloRecuperado.appendChild(linhas)
+      trianguloRecuperado.appendChild(criarElemento("line"))
     }
     linhasDoHtml = document.querySelectorAll(".line");
+    return true;
   }  
   
   function atualizarContador() {
     if (typeof (Storage) != "undefined") {
       if(localStorage.count !== undefined) {
-        let count = parseInt(localStorage.count);
-        count+=1;
-        localStorage.count = count;
-        document.getElementById("count").innerHTML = count;
+        localStorage.count = parseInt(localStorage.count) +1;
+        document.getElementById("count").innerHTML = localStorage.count;
       } else {
         localStorage.count = 1;
-        document.getElementById("count").innerHTML = 1;
+        document.getElementById("count").innerHTML = localStorage.count;
       }
     } else {
       document.write("Sem suporte para Web Storage");
     }  
+    return true;
   }
 
-  function preenchertriangulo(linhasDoHtml) {
+  function preencherTriangulo(linhasDoHtml) {
     for(let index = 0; index < linhasDoHtml.length; index += 1) {
       preencherLinha(linhasDoHtml[index]);
       ladoDireito += 1;
       ladoEsquerdo -= 1;
     }
+    atualizarContador();
   }
 
-  function criarBloco(className) {
+  function criarElemento(className) {
     let bloco = document.createElement("div");
     bloco.className = className;
     return bloco;
@@ -52,10 +49,10 @@ window.onload = function() {
   function preencherLinha(divLine) {
     for (let linha_colune = 1; linha_colune <= baseDaPiramide; linha_colune += 1) {
       if(linha_colune >= ladoEsquerdo && linha_colune <= ladoDireito) {
-        let blocoCriado = criarBloco("box");
+        let blocoCriado = criarElemento("box");
         divLine.appendChild(blocoCriado);
       } else {
-        divLine.appendChild(criarBloco("box-empty"));
+        divLine.appendChild(criarElemento("box-empty"));
       }
     }
   }
